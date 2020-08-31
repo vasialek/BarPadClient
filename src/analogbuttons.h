39bc28943ee3cffdc6d46f106a9bfe686a7538e9
Aleksej V <proglamer@gmail.com>
2020-08-31 22:27:39 +0300

    Class to get multi buttons clicks from A0 input.

---
+++ src/analogbuttons.h
@@ -0,0 +1,92 @@
+#ifndef ANALOGBUTTONS_H
+#define ANALOGBUTTONS_H
+
+#include <Arduino.h>
+
+class AnalogButtons
+{
+private:
+    unsigned char _inputPin;
+    
+    int _previousButton = -1;
+    unsigned int _thresholdMs = 140;
+    unsigned long _lastButtonPressed = 0;
+
+    // 3.3V
+    // int _maxAllowedValue = 1000;
+    // int _firstMinValue = 155;
+    // int _secondMinValue = 300;
+    // int _thirdMinValue = 740;
+
+    // 5V
+    int _maxAllowedValue = 1030;
+    int _firstMinValue = 215;
+    int _secondMinValue = 415;
+    int _thirdMinValue = 1020;
+
+    bool IsButtonReallyPressed(int buttonToCheck);
+    void DebugValue(int value);
+public:
+    AnalogButtons(unsigned char inputPin);
+    int GetPressedButton();
+};
+
+AnalogButtons::AnalogButtons(unsigned char inputPin)
+{
+    _inputPin = inputPin;
+}
+
+int AnalogButtons::GetPressedButton()
+{
+    int value = analogRead(_inputPin);
+    // DebugValue(value);
+    if (value > _maxAllowedValue)
+    {
+        return 0;
+    }
+    if (value >= _thirdMinValue)
+    {
+        return IsButtonReallyPressed(3) ? 3 : 0;
+    }
+    if (value >= _secondMinValue)
+    {
+        return IsButtonReallyPressed(2) ? 2 : 0;
+    }
+    if (value >= _firstMinValue)
+    {
+        return IsButtonReallyPressed(1) ? 1 : 0;
+    }
+
+    return 0;
+}
+
+bool AnalogButtons::IsButtonReallyPressed(int buttonToCheck)
+{
+    if (buttonToCheck != _previousButton)
+    {
+        _previousButton = buttonToCheck;
+        _lastButtonPressed = millis();
+        return true;
+    }
+    if (millis() - _lastButtonPressed > _thresholdMs)
+    {
+        _previousButton = buttonToCheck;
+        _lastButtonPressed = millis();
+        return true;
+    }
+
+    return false;
+}
+
+void AnalogButtons::DebugValue(int value)
+{
+    static int previousValue = 0;
+    if (abs(previousValue - value) > 10)
+    {
+        Serial.print("Read value: ");
+        Serial.println(value);
+        previousValue = value;
+    }
+}
+
+#endif
